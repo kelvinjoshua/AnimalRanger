00d3d86129683840eb68785a51c2f720a5d7502f
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-01 18:59:28 +0300

    Added private properties and constructor for Sightings classs

--- src/main/java/Models/Sightings.java
+++ src/main/java/Models/Sightings.java
@@ -1,4 +1,18 @@
 package Models;
+import org.sql2o.Connection;
+import java.util.List;
 
 public class Sightings {
+    private int animalId;
+    private String location;
+    private String ranger;
+    private int id;
+
+    /*Constructor*/
+    public Sightings (String ranger, String location, int animalId ) {
+        this.ranger = ranger;
+        this.location = location;
+        this.animalId = animalId;
+    }
+
 }

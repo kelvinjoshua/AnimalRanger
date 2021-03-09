1d49c51939ce9396b5a092f5d581fc55c34985cf
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-09 15:33:38 +0300

    new database update

--- src/main/java/Models/DB.java
+++ src/main/java/Models/DB.java
@@ -4,13 +4,14 @@ import org.sql2o.Sql2o;
 
 public class DB {
 
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "bale","kelvin23");
+  //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "bale","kelvin23");
+  String connectionString = "jdbc:postgresql://ec2-54-161-239-198.compute-1.amazonaws.com:5432/d2bjal4k2hqfo6";
+  Sql2o sql2o = new Sql2o(connectionString, "mergbiwnuandzr", "765882d2333bde1e79554e6f68a6b6ccfc409787de45807538983eed9461b4d8");
 
-    //public static Sql2o sql2o =new Sql2o("jdbc:postgresql://ec2-3-232-163-23.compute-1.amazonaws.com:5432/d5v2041urlm5gq","szitmvwhvwojvp","7de581677cc302a99344b0261b2b8fbe05f5c4fb341336d7c4ded0ab6e1eaf07");
-    //postgres://szitmvwhvwojvp:7de581677cc302a99344b0261b2b8fbe05f5c4fb341336d7c4ded0ab6e1eaf07@ec2-3-232-163-23.compute-1.amazonaws.com:5432/d5v2041urlm5gq
-
-    //   String connectionString = "jdbc:postgresql://ec2-204-236-228-169.compute-1.amazonaws.com:5432/d2gbuioks1j7sv"; //!
-//        Sql2o sql2o = new Sql2o(connectionString, "hjfgyjguaxciit", "ab84930a6217bbb75e971a6c1639efc2bff76d985dac6a24aefd16920d82155c"); //!
 }
 
 /*can be used by multiple classes setup connection before & after every test*/
+
+
+/*new uri*/
+//postgres://mergbiwnuandzr:765882d2333bde1e79554e6f68a6b6ccfc409787de45807538983eed9461b4d8@ec2-54-161-239-198.compute-1.amazonaws.com:5432/d2bjal4k2hqfo6

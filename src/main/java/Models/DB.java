9884b7848fea63ad0673061492fbb2cac0cd5f71
kelvinjoshua <kelvinbale78@gmail.com>
2021-03-09 15:47:35 +0300

    made sql2o static

--- src/main/java/Models/DB.java
+++ src/main/java/Models/DB.java
@@ -5,8 +5,8 @@ import org.sql2o.Sql2o;
 public class DB {
 
   //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "bale","kelvin23");
-  String connectionString = "jdbc:postgresql://ec2-54-161-239-198.compute-1.amazonaws.com:5432/d2bjal4k2hqfo6";
-  Sql2o sql2o = new Sql2o(connectionString, "mergbiwnuandzr", "765882d2333bde1e79554e6f68a6b6ccfc409787de45807538983eed9461b4d8");
+  static String connectionString = "jdbc:postgresql://ec2-54-161-239-198.compute-1.amazonaws.com:5432/d2bjal4k2hqfo6";
+  public static  Sql2o sql2o = new Sql2o(connectionString, "mergbiwnuandzr", "765882d2333bde1e79554e6f68a6b6ccfc409787de45807538983eed9461b4d8");
 
 }
 
